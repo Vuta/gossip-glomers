b0e55a51199fe0ef8b0a9d57ddb0cd5483cae13e
Tran Anh Vu <eyelight93@gmail.com>
2025-02-14 16:51:09 +0700

    5c: efficient Kafka-style log

--- kafka-style-log/go.mod
+++ kafka-style-log/go.mod
@@ -3,3 +3,5 @@ module gossip-glomers
 go 1.23.3
 
 require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20241204184542-016086a90274
+
+require bitbucket.org/pcastools/hash v1.0.5
--- kafka-style-log/main.go
+++ kafka-style-log/main.go
@@ -7,11 +7,44 @@ import (
 	"context"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	hash "bitbucket.org/pcastools/hash"
 )
+
+type SendEvent struct {
+	key string
+	value int
+}
+
+type State struct {
+ 	partitions map[string][]int
+}
 
 func main() {
 	n := maelstrom.NewNode()
-	kv := maelstrom.NewLinKV(n)
+	// We don't need LinKV service anymore since the routing ensure partitions will be written by their respectively leader node
+	kv := maelstrom.NewSeqKV(n)
+	state := &State{partitions: make(map[string][]int)}
+	sendCh := make(chan SendEvent)
+	doneCh := make(chan bool)
+
+	// this goroutine synchronize write requests
+	go func() {
+		ctx := context.Background()
+
+		for {
+			select {
+			case m := <-sendCh:
+				state.partitions[m.key] = append(state.partitions[m.key], m.value)
+				doneCh <- true
+
+				// Unfortunately the KV service doesn't provide a range read operation,
+				// so if we store each message in separate entry, poll PRC will issue a lot of reads to KV, which is really bad.
+				// For now writing the whole thing still yield good enough result, so I'm gonna keep it this way.
+				// TODO: Use segment to avoid writing the whole partition
+				kv.Write(ctx, m.key, state.partitions[m.key])
+			}
+		}
+	}()
 
 	n.Handle("send", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -19,22 +52,33 @@ func main() {
 			return err
 		}
 
-		ctx := context.Background()
-
-		partitionId := body["key"].(string) 
 		value := int(body["msg"].(float64))
-		offset := 0
-		for {
-			partition := []int{}
-			kv.ReadInto(ctx, partitionId, &partition)
-			err := kv.CompareAndSwap(ctx, partitionId, partition, append(partition, value), true)
-			if err == nil {
-				offset = len(partition)
-				break
+		partitionId := body["key"].(string)
+
+		// round robin keys to leader node, only works with stable network
+		leaderIndex := hash.ByteSlice([]byte(partitionId)) % uint32(len(n.NodeIDs()))
+		leader := n.NodeIDs()[leaderIndex]
+		if leader == n.ID() {
+			sendCh <- SendEvent{key: partitionId, value: value}
+
+			select {
+			case <-doneCh:
+				offset := len(state.partitions[partitionId]) - 1
+				n.Reply(msg, map[string]any{"type": "send_ok", "offset": offset})
 			}
+		} else {
+			// using SyncRPC is the correct way, but in this challenge we assume a stable network
+			n.RPC(leader, body, func(msg1 maelstrom.Message) error {
+				var body map[string]any
+				if err := json.Unmarshal(msg1.Body, &body); err != nil {
+					return err
+				}
+
+				return n.Reply(msg, map[string]any{"type": "send_ok", "offset": body["offset"]})
+			})
 		}
 
-		return n.Reply(msg, map[string]any{"type": "send_ok", "offset": offset})
+		return nil
 	})
 
 	n.Handle("poll", func(msg maelstrom.Message) error {
@@ -48,8 +92,15 @@ func main() {
 
 		for key, offset := range body["offsets"].(map[string]any) {
 			offset := int(offset.(float64))
+			leaderIndex := hash.ByteSlice([]byte(key)) % uint32(len(n.NodeIDs()))
+			leader := n.NodeIDs()[leaderIndex]
 			partition := []int{}
-			kv.ReadInto(ctx, key, &partition)
+
+			if leader == n.ID() {
+				partition = state.partitions[key]
+			} else {
+				kv.ReadInto(ctx, key, &partition)
+			}
 
 			if offset >= 0 && offset <= len(partition) {
 				endOffset := len(partition)
